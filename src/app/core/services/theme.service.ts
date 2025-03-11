dd1607a7f7b6d3b75664f0bd27716f27e4c903e4
esraaali138 <esraaali1999138@gmail.com>
2025-03-11 20:16:51 +0200

    handle date and status in order

--- src/app/core/services/theme.service.ts
+++ src/app/core/services/theme.service.ts
@@ -5,37 +5,30 @@ import { BehaviorSubject } from 'rxjs';
   providedIn: 'root',
 })
 export class ThemeService {
-  isDarkEnable = false;
-  private presentTheme = new BehaviorSubject<string>('light');
-  darkMode$ = this.presentTheme.asObservable();
-
   constructor() {
     this.loadTheme();
     this.subscribeToThemeChanges();
   }
-  private loadTheme() {
-    const savedTheme = localStorage.getItem('theme') || 'light';
-    if (savedTheme) {
-      this.presentTheme.next(savedTheme);
-    }
-  }
+  private presentTheme = new BehaviorSubject<string>('light');
+  darkMode$ = this.presentTheme.asObservable();
 
   changeTheme() {
-    const newTheme =
+    const themeValue =
       this.presentTheme.getValue() === 'light' ? 'dark' : 'light';
-    this.presentTheme.next(newTheme);
-    localStorage.setItem('theme', newTheme);
+    this.presentTheme.next(themeValue);
+    localStorage.setItem('theme', themeValue);
   }
 
   private applyTheme(theme: string) {
     document.documentElement.classList.remove('light', 'dark');
     document.documentElement.classList.add(theme);
-    this.isDarkEnable = theme === 'dark';
   }
 
   private subscribeToThemeChanges() {
-    this.darkMode$.subscribe((theme) => {
-      this.applyTheme(theme);
-    });
+    this.darkMode$.subscribe((theme) => this.applyTheme(theme));
+  }
+  private loadTheme() {
+    const savedTheme = localStorage.getItem('theme') || 'light';
+    this.presentTheme.next(savedTheme);
   }
 }
--- src/app/feature/orders/components/orders-table/orders-table.component.ts
+++ src/app/feature/orders/components/orders-table/orders-table.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Order } from '../../models/orders';
+import { OrdersService } from '../../services/orders.service';
 
 @Component({
   selector: 'app-orders-table',
@@ -10,7 +11,34 @@ import { Order } from '../../models/orders';
 export class OrdersTableComponent {
   data: string = '';
   @Input() orders!: Order[];
-  getRandomDate() {
-  return  this.data = new Date().toISOString().split('T')[0];
+
+  constructor(private orderService: OrdersService) {}
+
+  ngOnInit() {
+    this.orderService.getOrders().subscribe((orders) => {
+      console.log('orders', orders);
+      this.orders = orders.map((order) => ({
+        ...order,
+        products: order.products.map((product) => ({
+          ...product,
+          status: this.getRandomStatus(),
+          data: this.generateRandomDate(),
+        })),
+      }));
+    });
+  }
+
+  generateRandomDate(): string {
+    const currentDate = new Date().getTime();
+    const prevDate = new Date(2023, 2, 12).getTime();
+    const randomDate = new Date(
+      prevDate + Math.random() * (prevDate - currentDate)
+    );
+    return randomDate.toISOString().split('T')[0];
+  }
+
+  getRandomStatus(): string {
+    const statuses = ['Pending', 'Approved', 'In Process', 'Cancelled'];
+    return statuses[Math.floor(Math.random() * statuses.length)];
   }
 }
